4066af4d5404383671aa5ac7a3c1f1569e9af63a
Hadi Ramadan <hfr002@live.aul.edu.lb>
2020-11-20 13:12:58 +0200

    Created a form that contains an image, username, password and button.

---
+++ src/forms/Main.java
@@ -0,0 +1,75 @@
+package forms;
+	
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+
+
+
+public class Main extends Application {
+	
+	private static final String imageURL = "https://picsum.photos/200";
+
+	ImageView creatImageView(){
+		Image image = new Image(imageURL, 200, 200, true, true);
+		ImageView imageView = new ImageView(image);
+		return imageView;
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		
+		grid.getChildren().add(creatImageView());
+
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
+
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1);
+
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
+		
+		Button btn = new Button("Sign in");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(btn);
+		grid.add(hbBtn, 1, 4);
+
+		Scene scene = new Scene(grid, 500, 500);
+		primaryStage.setScene(scene);
+		
+		primaryStage.setTitle("JavaFx Form with image");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
